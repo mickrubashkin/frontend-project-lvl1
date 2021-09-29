a07bdf46ab3c71a8dacaab946da57df8b72335c7
MickRubashkin <mikhail.rubashkin@gmail.com>
2021-09-29 13:23:50 +0200

    fix(isPrime): add failure case for 0 and 1

--- src/games/prime-game.js
+++ src/games/prime-game.js
@@ -4,13 +4,10 @@ import gameLoop from '../game-loop.js';
 const gameRules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = (num) => {
-  // const PRIMES_TO_101 = [
-  //   2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
-  //   31, 37, 41, 43, 47, 53, 59, 61, 67, 71,
-  //   73, 79, 83, 89, 97, 101,
-  // ];
+  if (num < 2) {
+    return false;
+  }
 
-  // return PRIMES_TO_101.includes(num);
   for (let i = 2; i < num ** 0.5; i += 1) {
     if (num % i === 0) {
       return false;
@@ -21,8 +18,9 @@ const isPrime = (num) => {
 };
 
 const generateQuestionAndAnswer = () => {
-  const question = getRandomNumber();
-  const answer = isPrime(question) ? 'yes' : 'no';
+  const num = getRandomNumber();
+  const question = `${num}`;
+  const answer = isPrime(num) ? 'yes' : 'no';
 
   return [question, answer];
 };
