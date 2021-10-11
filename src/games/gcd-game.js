ee161326ff27d803b52c93474e50db1b95e4ed00
MikkRou <mikhail.rubashkin@gmail.com>
2021-10-11 17:07:24 +0200

    fix(gcd-game): simplified gcd algorithm

--- src/games/gcd-game.js
+++ src/games/gcd-game.js
@@ -3,21 +3,13 @@ import playGame from '../index.js';
 
 const rules = 'Find the greatest common divisor of given numbers.';
 
-const getGcd = (a, b) => {
-  // TODO (gcd): И попробуй найти еще более элегантное решение для функции GCD
+const getGcd = (a, b) => b === 0 ? a : getGcd(b, a % b);
 
-  // Euclidean algorithm
-  // https://en.wikipedia.org/wiki/Greatest_common_divisor
-
-  const small = Math.min(a, b);
-  const big = Math.max(a, b);
-
-  if (small === 0) {
-    return big;
-  }
-
-  return getGcd(small, big % small);
-};
+// function gcd(a, b)
+// if b = 0
+//         return a
+// else
+//   return gcd(b, a mod b)
 
 const generateRoundData = () => {
   const num1 = getRandomInteger(1, 100);
